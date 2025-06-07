a1f281cc840d2251af6e919d7090c515baa66a01
anurag402 <anuragsaran472@gmail.com>
2025-06-08 01:19:08 +0530

    maxduration

--- src/app/api/process-meeting/route.ts
+++ src/app/api/process-meeting/route.ts
@@ -10,7 +10,7 @@ const bodyParser = z.object({
   projectId: z.string(),
 });
 
-export const runtime = 60;
+export const maxDuration = 60; 
 
 export async function POST(req: NextRequest) {
   const { userId } = await auth();
